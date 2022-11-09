9c4f537bdd4c795dbed23cb8e47d75965e1ce9cf
Anandhu Rajan <anandhurajan56@gmail.com>
2022-11-09 11:05:41 +0530

    styled pop-up and validation form..next is js

--- app.js
+++ app.js
@@ -6,3 +6,24 @@ menu.addEventListener('click',() =>{
      menu.classList.toggle('is-active');
      menuList.classList.toggle('active');   
 });
+// modal items
+
+const modal = document.getElementById('email-modal');
+const openBtn = document.querySelector('.main-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+// Click Event
+
+openBtn.addEventListener('click',()=> {
+     modal.style.display ='block';
+});
+
+closeBtn.addEventListener('click',()=>{
+     modal.style.display = 'none';
+});
+
+window.addEventListener('click',(e) =>{
+     if( e.target ===modal) {
+          modal.style.display ='none';
+     }
+});
